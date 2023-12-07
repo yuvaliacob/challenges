40c0450011e2d95c45e2a9b6dfd82ed673c99f24
Yuval Iacob <burpie@gmail.com>
2023-12-07 13:16:10 +0100

    Solve JS Tip Calculator challenge

---
+++ js/js-tip-calculator/index.js
@@ -0,0 +1,15 @@
+console.clear();
+
+// Cost of the meal
+let mealCost = 50;
+
+// Percentage of tip
+let tipPercentage = 0.1;
+
+// Tip calculation
+let tipAmount = mealCost * tipPercentage;
+
+// Total meal cost
+let totalCost = mealCost + tipAmount;
+
+console.log("// The total cost of your bill is:", totalCost, "euros");
