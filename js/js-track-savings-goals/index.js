18c7e8d3beeaa4cc085a2ddc58401f9e7be8d03f
Yuval Iacob <burpie@gmail.com>
2023-12-07 14:06:29 +0100

    Solve Track Your Savings Goals challenge

---
+++ js/js-track-savings-goals/index.js
@@ -0,0 +1,21 @@
+console.clear();
+
+// Initial savings balance
+let startingBalance = 1000;
+
+// Montly savings
+let monthlyContribution = 100;
+
+// Duration in months
+let months = 12;
+
+// Calculation of total savings
+const totalSavings = startingBalance + monthlyContribution * months;
+
+const euroSymbol = "€";
+
+console.log(
+  "After 12 months of saving, you will have",
+  euroSymbol + totalSavings,
+  "in your account."
+);
