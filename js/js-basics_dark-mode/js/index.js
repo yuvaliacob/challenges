04425939f5f1c7ed8c222220ca8219c52de5bc34
Yuval Iacob <burpie@gmail.com>
2023-12-10 16:11:26 +0100

    Solve JS Dark Mode challenge

--- js/js-basics_dark-mode/js/index.js
+++ js/js-basics_dark-mode/js/index.js
@@ -1,3 +1,32 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+const buttonDarkMode = document.querySelector('[data-js="dark-mode-button"]');
+
+const buttonLightMode = document.querySelector('[data-js="light-mode-button"]');
+
+const buttonToggle = document.querySelector('[data-js="toggle-button"]');
+
+buttonDarkMode.addEventListener("click", () => {
+  bodyElement.classList.add("dark");
+});
+
+buttonLightMode.addEventListener("click", () => {
+  bodyElement.classList.remove("dark");
+});
+
+buttonToggle.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+});
+
+// const firstChoiceButton = document.querySelector('[data-js="choice-button-1"]');
+// const secondChoiceButton = document.querySelector(
+//   '[data-js="choice-button-2"]'
+// );
+
+// firstChoiceButton.addEventListener("click", () => {
+//   firstChoiceButton.classList.add("active-choice");
+//   secondChoiceButton.classList.remove("active-choice");
+//   console("Selected first choice");
+// });
