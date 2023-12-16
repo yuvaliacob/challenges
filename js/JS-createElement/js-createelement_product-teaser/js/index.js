29425b22fae02a61d70ed60ccfc2aa3cf99d0eda
Yuval Iacob <burpie@gmail.com>
2023-12-17 00:09:20 +0100

    Solution: Product Teaser challenge

--- js/JS-createElement/js-createelement_product-teaser/js/index.js
+++ js/JS-createElement/js-createelement_product-teaser/js/index.js
@@ -9,3 +9,53 @@ const category3 = "Plankton Diet";
 const price = "149,99 €";
 const imageSrc =
   "https://unsplash.com/photos/3VOTHTrE614/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjU5NTM3NTA2&force=true&w=640";
+
+/* 
+1. Make a second product teaser and append to the page
+2 Use .innerHTML to generate the HTML elements for the teaser. Use string interpolation to include the given data into the HTML.
+3. Add an event listener to the "buy" button of the second product teaser. When clicking the button, the product's name and price should be logged to the console.
+*/
+
+const angelFishCard = document.createElement("article");
+angelFishCard.classList.add("product");
+document.body.append(angelFishCard);
+
+angelFishCard.innerHTML = `
+
+<div class="product__body">
+        <div class="product__text-container">
+          <h2 class="product__name">${name}</h2>
+          <ul class="product__categories">
+            <li class="product__category">${category1}</li>
+            <li class="product__category">${category2}</li>
+            <li class="product__category">${category3}</li>
+          </ul>
+          <p class="product__description">
+            ${description}
+          </p>
+        </div>
+        <div class="product__image-container">
+          <img
+            class="product__image"
+            src=${imageSrc}
+            alt=""
+          />
+        </div>
+      </div>
+      <footer class="product__footer">
+        <span class="product__price">${price}</span>
+        <button type="button" class="product__buy-button" data-js="second_buy_button">Buy</button>
+      </footer>
+      
+`;
+
+const secondBuyButton = document.querySelector('[data-js="second_buy_button"]');
+
+secondBuyButton.addEventListener("click", () => {
+  console.log(
+    `
+    Product name: ${name}
+    Price: ${price}
+    `
+  );
+});
