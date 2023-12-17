317fc36c69a1332a0688449c573d07f3c41cfe2c
Yuval Iacob <burpie@gmail.com>
2023-12-17 11:17:37 +0100

    Solution: JS While challenge

--- js/JS-Loops/js-loops_while-random-number/index.js
+++ js/JS-Loops/js-loops_while-random-number/index.js
@@ -7,6 +7,12 @@ let count = 0;
 
 // --v-- write/change code here --v--
 
+while (number <= 0.9) {
+  console.log(number);
+  number = Math.random();
+  count++;
+}
+
 // --^-- write/change code here --^--
 
 console.log(
