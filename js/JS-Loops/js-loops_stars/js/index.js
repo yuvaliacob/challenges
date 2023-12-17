93d5eef4cf6bb61189ec231190efb1da20acfd1d
Yuval Iacob <burpie@gmail.com>
2023-12-17 18:44:58 +0100

    Solution: JS Loops Stars challenge

--- js/JS-Loops/js-loops_stars/js/index.js
+++ js/JS-Loops/js-loops_stars/js/index.js
@@ -2,13 +2,26 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
-
   //--v-- your code here --v--
+  for (let counter = 1; counter <= 5; counter++) {
+    const star = document.createElement("img");
 
+    if (filledStars >= counter) {
+      star.src = "assets/star-filled.svg";
+    } else {
+      star.src = "assets/star-empty.svg";
+    }
+
+    star.addEventListener("click", () => {
+      renderStars(counter);
+    });
+
+    starContainer.append(star);
+  }
   //--^-- your code here --^--
 }
 
-renderStars();
+renderStars(0);
