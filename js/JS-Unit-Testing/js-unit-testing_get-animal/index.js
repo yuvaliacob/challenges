1d6aaab1741adf8bc83db0846b1bb63808a3a01e
Yuval Iacob <burpie@gmail.com>
2024-01-09 14:56:29 +0100

    Solution: JS I like animals, don't I? challenge

--- js/JS-Unit-Testing/js-unit-testing_get-animal/index.js
+++ js/JS-Unit-Testing/js-unit-testing_get-animal/index.js
@@ -1,1 +1,8 @@
-export function getAnimal() {}
+export function getAnimal(animal) {
+  if (animal === "cats") {
+    return "I totally love cats!";
+  } else if (typeof animal === "string") {
+    return `I like ${animal}!`;
+  }
+  return "I do not like animals at all!";
+}
