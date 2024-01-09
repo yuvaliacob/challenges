872e7058b351a0a2e14fafe73d970dcb3eb89790
Yuval Iacob <burpie@gmail.com>
2024-01-09 15:29:59 +0100

    JS Unit Testing - Math Functions - tests of SUBTRACT

--- js/JS-Unit-Testing/js-unit-testing_math-functions/index.test.js
+++ js/JS-Unit-Testing/js-unit-testing_math-functions/index.test.js
@@ -18,3 +18,14 @@ test("returns a value close to 0.3 if called with add(0.1, 0.2)", () => {
   const result = add(0.1, 0.2);
   expect(result).toBeCloseTo(0.3);
 });
+
+// Subtract test
+test("returns 10 if called with subtract(15, 5)", () => {
+  const result = subtract(15, 5);
+  expect(result).toBe(10);
+});
+
+test("returns a negative value if the second argument is greater than the first one", () => {
+  const result = subtract(5, 15);
+  expect(result).toBeLessThan(0);
+});
