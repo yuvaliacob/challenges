306e1860c054b77188dec65c9d6562667c7b8ca9
Yuval Iacob <burpie@gmail.com>
2024-01-09 15:39:37 +0100

    JS Unit Testing - Math Functions - tests of MULTIPLY

--- js/JS-Unit-Testing/js-unit-testing_math-functions/index.test.js
+++ js/JS-Unit-Testing/js-unit-testing_math-functions/index.test.js
@@ -19,7 +19,7 @@ test("returns a value close to 0.3 if called with add(0.1, 0.2)", () => {
   expect(result).toBeCloseTo(0.3);
 });
 
-// Subtract test
+// Subtract tests
 test("returns 10 if called with subtract(15, 5)", () => {
   const result = subtract(15, 5);
   expect(result).toBe(10);
@@ -28,4 +28,25 @@ test("returns 10 if called with subtract(15, 5)", () => {
 test("returns a negative value if the second argument is greater than the first one", () => {
   const result = subtract(5, 15);
   expect(result).toBeLessThan(0);
+});
+
+// Multiply tests
+test("returns 8 if called with multiply(2, 4)", () => {
+  const result = multiply(2, 4);
+  expect(result).toBe(8);
+});
+
+test("returns a negative value if only the first argument is negative", () => {
+  const result = multiply(-1, 1);
+  expect(result).toBeLessThan(0);
+});
+
+test("returns a negative value if only the second argument is negative", () => {
+  const result = multiply(1, -1);
+  expect(result).toBeLessThan(0);
+});
+
+test("returns a positive value if called with two negative arguments", () => {
+  const result = multiply(-1, -1);
+  expect(result).toBeGreaterThan(0);
 });
