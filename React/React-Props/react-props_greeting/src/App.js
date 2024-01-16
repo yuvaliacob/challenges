4effe8043116f557ff9daa6e9e9226f3dc17a1d9
Yuval Iacob <burpie@gmail.com>
2024-01-16 16:23:38 +0100

    React Greeting challenge Part 1

--- React/React-Props/react-props_greeting/src/App.js
+++ React/React-Props/react-props_greeting/src/App.js
@@ -1,5 +1,9 @@
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Greeting name="Donna" />;
+}
+
+function Greeting({ name }) {
+  return <h1>Hello, {name}!</h1>;
 }
