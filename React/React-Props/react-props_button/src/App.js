192dd327c65dca8b2093332e8ea48efaa92625fc
Yuval Iacob <burpie@gmail.com>
2024-01-16 17:00:52 +0100

    React Buttons challenge Part 1

--- React/React-Props/react-props_button/src/App.js
+++ React/React-Props/react-props_button/src/App.js
@@ -2,5 +2,13 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Button color="red" disabled={false} text="Some text." />;
+}
+
+function Button({ color, disabled, text }) {
+  return (
+    <button style={{ color: color }} disabled={disabled}>
+      {text}
+    </button>
+  );
 }
