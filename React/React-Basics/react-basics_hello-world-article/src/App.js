ae2473e3fbd768c466bc727b7ddc45f04a60dd0d
Yuval Iacob <burpie@gmail.com>
2024-01-16 13:29:31 +0100

    Solution: Hello World Article challenge

--- React/React-Basics/react-basics_hello-world-article/src/App.js
+++ React/React-Basics/react-basics_hello-world-article/src/App.js
@@ -2,5 +2,14 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return HellowWorldArticle();
+}
+
+function HellowWorldArticle() {
+  return (
+    <article>
+      <h1>Title</h1>
+      <p>Some text.</p>
+    </article>
+  );
 }
