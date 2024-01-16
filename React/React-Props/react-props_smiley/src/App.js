54b7991228774fab316e7478db5764e6655c6393
Yuval Iacob <burpie@gmail.com>
2024-01-16 16:49:20 +0100

    Solution: Smile ;) challenge

--- React/React-Props/react-props_smiley/src/App.js
+++ React/React-Props/react-props_smiley/src/App.js
@@ -2,5 +2,9 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Smiley isHappy={true} />;
+}
+
+function Smiley({ isHappy }) {
+  return <p>{isHappy ? "😃" : "😔"}</p>;
 }
