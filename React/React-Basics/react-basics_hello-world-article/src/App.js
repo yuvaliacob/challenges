0d9e453abca74f080723a80bf83aab61c0308d08
Yuval Iacob <burpie@gmail.com>
2024-01-16 13:58:19 +0100

    Solution: React Hello World Article challenge

--- React/React-Basics/react-basics_hello-world-article/src/App.js
+++ React/React-Basics/react-basics_hello-world-article/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return HellowWorldArticle();
+  return <HellowWorldArticle />;
 }
 
 function HellowWorldArticle() {
