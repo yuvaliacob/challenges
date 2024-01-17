a8eb9b972c859ddcda1b150d93a6ee1844b5a9e7
Yuval Iacob <burpie@gmail.com>
2024-01-17 13:47:14 +0100

    Upload React with Arrays challenges

---
+++ React/React-with-Arrays/react-with-arrays_fruits/src/App.js
@@ -0,0 +1,11 @@
+import Card from "./components/Card";
+
+export default function App() {
+  const fruits = [];
+
+  return (
+    <div className="app">
+      <Card name="🍌 banana" />
+    </div>
+  );
+}
