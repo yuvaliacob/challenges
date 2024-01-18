db7f1e5db87a89ab30b69bd9a2d00713a4130421
Yuval Iacob <burpie@gmail.com>
2024-01-18 15:28:28 +0100

    Solution: Fix Conditionally Rendered useState Hook Challenge

--- React/React-State-2/react-state-2_conditional-usestate/src/App.js
+++ React/React-State-2/react-state-2_conditional-usestate/src/App.js
@@ -4,6 +4,8 @@ import { useState } from "react";
 
 export default function App() {
   const [showMessage, setShowMessage] = useState(false);
+  const [message, setMessage] = useState("Secret message incoming...");
+
   if (!showMessage) {
     return (
       <div className="container">
@@ -23,8 +25,6 @@ export default function App() {
       </div>
     );
   }
-
-  const [message, setMessage] = useState("Secret message incoming...");
 
   return (
     <div className="container">
