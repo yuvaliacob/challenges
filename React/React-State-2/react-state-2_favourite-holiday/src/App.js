1dc17e6d6030404bb03592eb81c9d09926d9a095
Yuval Iacob <burpie@gmail.com>
2024-01-18 13:50:10 +0100

    Solution: Favourite Holiday Form challenge

--- React/React-State-2/react-state-2_favourite-holiday/src/App.js
+++ React/React-State-2/react-state-2_favourite-holiday/src/App.js
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import React from "react";
 import "./styles.css";
 
 export default function App() {
+  const [holiday, setHoliday] = useState("");
+  const [date, setDate] = useState("");
+
   function handleSubmit(event) {
     event.preventDefault();
+    // get the value from the form
+    const formElements = event.target.elements;
+
+    console.log(formElements.holiday.value);
+    console.log(formElements.date.value);
+
+    setHoliday(formElements.holiday.value);
+    setDate(formElements.date.value);
+    // console.log("submit");
+
+    event.target.reset();
+    event.target.elements.holiday.focus();
   }
 
   return (
@@ -30,10 +46,10 @@ export default function App() {
       </form>
       <h2>Output of Submitted Data</h2>
       <p>
-        Favourite Holiday: <span className="output">New Year</span>
+        Favourite Holiday: <span className="output">{holiday}</span>
       </p>
       <p>
-        Date: <span className="output">Well...</span>
+        Date: <span className="output">{date}</span>
       </p>
     </div>
   );
