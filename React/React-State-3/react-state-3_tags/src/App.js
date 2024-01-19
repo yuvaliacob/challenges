c0b7c3e10fac47a1a9d9e6a8a0d07e8a9586da62
Yuval Iacob <burpie@gmail.com>
2024-01-19 11:19:46 +0100

    Add new tag

--- React/React-State-3/react-state-3_tags/src/App.js
+++ React/React-State-3/react-state-3_tags/src/App.js
@@ -6,9 +6,16 @@ import List from "./components/List/index.js";
 export default function App() {
   const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
 
+  // In the App component, write a handleAddTag function which receives one parameter, e.g. newTag.
+  function handleAddTag(newTag) {
+    // console.log(newTag);
+    // Call the state setter function and add the new tag to the current state.
+    setTags([...tags, newTag]);
+  }
+
   return (
     <main className="app">
-      <Form />
+      <Form onAddTag={handleAddTag} />
       <List tags={tags} />
     </main>
   );
