482a612df7f773f48016f80ffa45606e9bc0c025
Yuval Iacob <burpie@gmail.com>
2024-01-22 11:46:44 +0100

    Solution: Journal App with Local Storage Challenge

--- React/React-with-Local-Storage/react-with-local-storage_journal-app-with-local-storage/src/App.js
+++ React/React-with-Local-Storage/react-with-local-storage_journal-app-with-local-storage/src/App.js
@@ -1,10 +1,11 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
 import EntriesSection from "./components/EntriesSection";
 import EntryForm from "./components/EntryForm";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { uid } from "uid";
+import useLocalStorageState from "use-local-storage-state";
 
 const initialEntries = [
   {
@@ -37,8 +38,14 @@ const initialEntries = [
 ];
 
 function App() {
-  const [entries, setEntries] = useState(initialEntries);
-  const [filter, setFilter] = useState("all"); // "all" or "favorites"
+  // const [entries, setEntries] = useState(initialEntries);
+  // const [filter, setFilter] = useState("all"); // "all" or "favorites"
+  const [entries, setEntries] = useLocalStorageState("entries", {
+    defaultValue: [...initialEntries],
+  });
+  const [filter, setFilter] = useLocalStorageState("filter", {
+    defaultValue: "all",
+  });
 
   function handleAddEntry(newEntry) {
     const date = new Date().toLocaleDateString("en-us", {
