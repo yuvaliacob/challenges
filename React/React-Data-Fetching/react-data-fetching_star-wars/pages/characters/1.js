240c60202e89034d37a4dfaa9b8cf371b196671e
Yuval Iacob <burpie@gmail.com>
2024-01-30 12:36:07 +0100

    Add Fetching with SWR to character 1

--- React/React-Data-Fetching/react-data-fetching_star-wars/pages/characters/1.js
+++ React/React-Data-Fetching/react-data-fetching_star-wars/pages/characters/1.js
@@ -1,17 +1,40 @@
 import Card from "../../components/Card";
 import Layout from "../../components/Layout";
+import useSWR from "swr";
+
+const fetcher = async (id) => {
+  const res = await fetch(`https://swapi.dev/api/people/${id}`);
+
+  if (!res.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+
+  return res.json();
+};
+
+const id = 1;
 
 export default function Character() {
-  const id = 1;
+  const { data, error, isLoading } = useSWR("/api/user", () => fetcher(id));
+
+  console.log("Fetched character data: ", data);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  const { name, height, eye_color, birth_year } = data;
 
   return (
     <Layout>
       <Card
         id={id}
-        name={"Luke Skywalker"}
-        height={172}
-        eyeColor={"blue"}
-        birthYear={"19BBY"}
+        name={name}
+        height={height}
+        eyeColor={eye_color}
+        birthYear={birth_year}
       />
     </Layout>
   );
