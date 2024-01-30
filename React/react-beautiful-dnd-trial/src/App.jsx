76372597e894138aacea30320dd3264beb03bc77
Yuval Iacob <burpie@gmail.com>
2024-01-30 19:17:18 +0100

    Add react-beautiful-dnd library and relevant code to list

--- React/react-beautiful-dnd-trial/src/App.jsx
+++ React/react-beautiful-dnd-trial/src/App.jsx
@@ -1,16 +1,51 @@
-import { DragDropContext } from "react-beautiful-dnd";
-import "./App.css";
+import { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-function App() {
+const App = () => {
+  const [listItems, setListItems] = useState([
+    { id: "1", content: "Drag and drop me" },
+    { id: "2", content: "Drag and drop me" },
+    { id: "3", content: "Drag and drop me" },
+    { id: "4", content: "Drag and drop me" },
+    { id: "5", content: "Drag and drop me" },
+  ]);
+
+  const onDragEnd = (result) => {
+    if (!result.destination) {
+      return;
+    }
+
+    const reorderedItems = Array.from(listItems);
+    const [removed] = reorderedItems.splice(result.source.index, 1);
+    reorderedItems.splice(result.destination.index, 0, removed);
+
+    setListItems(reorderedItems);
+  };
+
   return (
-    <ul>
-      <li>Drag and drop me</li>
-      <li>Drag and drop me</li>
-      <li>Drag and drop me</li>
-      <li>Drag and drop me</li>
-      <li>Drag and drop me</li>
-    </ul>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="list">
+        {(provided) => (
+          <ul {...provided.droppableProps} ref={provided.innerRef}>
+            {listItems.map((item, index) => (
+              <Draggable key={item.id} draggableId={item.id} index={index}>
+                {(provided) => (
+                  <li
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    ref={provided.innerRef}
+                  >
+                    {item.content}
+                  </li>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </ul>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
-}
+};
 
 export default App;
