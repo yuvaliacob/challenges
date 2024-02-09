3a9fe441dc707d962a370a111f3b97913e9c3ae1
Yuval Iacob <burpie@gmail.com>
2024-02-09 11:41:10 +0100

    Solution: Challenges: Backend API Routes: Random Character

--- Backend/Backend-API-Routes/backend-api-routes_random-character/pages/index.js
+++ Backend/Backend-API-Routes/backend-api-routes_random-character/pages/index.js
@@ -1,3 +1,30 @@
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const { data, isLoading } = useSWR(`/api/random-character`, fetcher);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+  console.log("Fetched: ", data);
+
+  if (!data) {
+    return;
+  }
+
+  const { firstName, lastName, twitterName, geohash } = data;
+
+  return (
+    <>
+      <h2>Fetched character data:</h2>
+      <ul>
+        <li>First name: {firstName}</li>
+        <li>Last name: {lastName}</li>
+        <li>Twitter handle: {twitterName}</li>
+        <li>Geohash: {geohash}</li>
+      </ul>
+    </>
+  );
 }
