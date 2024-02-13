bf0154293dee887a1c19fa9fe3d8d6882c592b08
Yuval Iacob <burpie@gmail.com>
2024-02-13 10:50:22 +0100

    Refactor the ProductForm component

--- Backend/Backend-Update-and-Delete/backend-update-and-delete_products/components/ProductForm/index.js
+++ Backend/Backend-Update-and-Delete/backend-update-and-delete_products/components/ProductForm/index.js
@@ -1,35 +1,9 @@
 import { StyledForm, StyledHeading, StyledLabel } from "./ProductForm.styled";
 import { StyledButton } from "../Button/Button.styled";
-import useSWR from "swr";
 
-export default function ProductForm() {
-  const { mutate } = useSWR("/api/products");
-
-  async function handleSubmit(event) {
-    event.preventDefault();
-
-    const formData = new FormData(event.target);
-    const productData = Object.fromEntries(formData);
-
-    const response = await fetch("/api/products", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(productData),
-    });
-
-    if (!response.ok) {
-      console.error(response.status);
-      return;
-    }
-
-    mutate();
-    event.target.reset();
-  }
-
+export default function ProductForm({ onSubmit }) {
   return (
-    <StyledForm onSubmit={handleSubmit}>
+    <StyledForm onSubmit={onSubmit}>
       <StyledHeading>Add a new Fish</StyledHeading>
       <StyledLabel htmlFor="name">
         Name:
--- Backend/Backend-Update-and-Delete/backend-update-and-delete_products/pages/api/products/index.js
+++ Backend/Backend-Update-and-Delete/backend-update-and-delete_products/pages/api/products/index.js
@@ -1,8 +1,34 @@
 import dbConnect from "../../../db/connect";
 import Product from "../../../db/models/Product";
+import useSWR from "swr";
 
 export default async function handler(request, response) {
   await dbConnect();
+
+  const { mutate } = useSWR("/api/products");
+
+  async function handleAddProduct(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+
+    const response = await fetch("/api/products", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+
+    if (!response.ok) {
+      console.error(response.status);
+      return;
+    }
+
+    mutate();
+    event.target.reset();
+  }
 
   if (request.method === "GET") {
     const products = await Product.find();
