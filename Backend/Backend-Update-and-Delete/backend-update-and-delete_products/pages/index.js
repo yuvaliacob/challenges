b30bc02a7c038c974d3ab6e7358f23ffc01024e5
Yuval Iacob <burpie@gmail.com>
2024-02-13 11:30:53 +0100

    Refactor the ProductForm component

--- Backend/Backend-Update-and-Delete/backend-update-and-delete_products/pages/index.js
+++ Backend/Backend-Update-and-Delete/backend-update-and-delete_products/pages/index.js
@@ -1,6 +1,7 @@
 import ProductList from "../components/ProductList";
 import styled from "styled-components";
 import ProductForm from "../components/ProductForm";
+import useSWR from "swr";
 
 const Heading = styled.h1`
   text-align: center;
@@ -8,6 +9,31 @@ const Heading = styled.h1`
 `;
 
 export default function HomePage() {
+  const { mutate } = useSWR("/api/products");
+
+  async function handleAddProduct(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+
+    const response = await fetch("/api/products", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+
+    if (!response.ok) {
+      console.error(response.status);
+      return;
+    }
+
+    mutate();
+    event.target.reset();
+  }
+
   return (
     <>
       <Heading>
@@ -16,7 +42,7 @@ export default function HomePage() {
         </span>
         Fish Shop
       </Heading>
-      <ProductForm />
+      <ProductForm onSubmit={handleAddProduct} />
       <hr />
       <ProductList />
     </>
