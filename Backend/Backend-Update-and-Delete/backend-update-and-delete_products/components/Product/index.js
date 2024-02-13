585f9683945195c7e079d0026f4be5ef3a8e2b2c
Yuval Iacob <burpie@gmail.com>
2024-02-13 11:04:55 +0100

    More updates but I'm deleting this and restarting

--- Backend/Backend-Update-and-Delete/backend-update-and-delete_products/components/Product/index.js
+++ Backend/Backend-Update-and-Delete/backend-update-and-delete_products/components/Product/index.js
@@ -3,12 +3,34 @@ import { useRouter } from "next/router";
 import { ProductCard } from "./Product.styled";
 import { StyledLink } from "../Link/Link.styled";
 import Comments from "../Comments";
+import { useState } from "react";
+import { StyledButton } from "../Button/Button.styled";
+import ProductForm from "../ProductForm";
 
 export default function Product() {
+  const [isEditMode, setIsEditMode] = useState(false);
   const router = useRouter();
   const { id } = router.query;
 
-  const { data, isLoading } = useSWR(`/api/products/${id}`);
+  const { data, isLoading, mutate } = useSWR(`/api/products/${id}`);
+
+  async function handleEditProduct(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+
+    if (response.ok) {
+      mutate();
+    }
+  }
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -19,14 +41,25 @@ export default function Product() {
   }
 
   return (
-    <ProductCard>
-      <h2>{data.name}</h2>
-      <p>Description: {data.description}</p>
-      <p>
-        Price: {data.price} {data.currency}
-      </p>
-      {data.reviews.length > 0 && <Comments reviews={data.reviews} />}
-      <StyledLink href="/">Back to all</StyledLink>
-    </ProductCard>
+    <>
+      <ProductCard>
+        <h2>{data.name}</h2>
+        <p>Description: {data.description}</p>
+        <p>
+          Price: {data.price} {data.currency}
+        </p>
+        {data.reviews.length > 0 && <Comments reviews={data.reviews} />}
+        <StyledLink href="/">Back to all</StyledLink>
+        <StyledButton
+          type="submit"
+          onClick={() => {
+            setIsEditMode(!isEditMode);
+          }}
+        >
+          Edit
+        </StyledButton>
+      </ProductCard>
+      {isEditMode && <ProductForm />}
+    </>
   );
 }
