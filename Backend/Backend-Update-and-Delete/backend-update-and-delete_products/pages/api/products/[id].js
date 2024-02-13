8416b0d23ab1b3145ba6a80ca9d424b70540c4bd
Yuval Iacob <burpie@gmail.com>
2024-02-13 11:27:46 +0100

    Add a PUT route (product details page)

--- Backend/Backend-Update-and-Delete/backend-update-and-delete_products/pages/api/products/[id].js
+++ Backend/Backend-Update-and-Delete/backend-update-and-delete_products/pages/api/products/[id].js
@@ -14,4 +14,16 @@ export default async function handler(request, response) {
 
     response.status(200).json(product);
   }
+
+  if (request.method === "PUT") {
+    try {
+      const updatedProduct = request.body;
+
+      await Product.findByIdAndUpdate(id, updatedProduct);
+
+      response.status(200).json({ status: "Product successfully updated." });
+    } catch (error) {
+      response.status(500).json({ error: "Failed to update the product." });
+    }
+  }
 }
